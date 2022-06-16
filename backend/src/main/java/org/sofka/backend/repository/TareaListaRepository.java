aca4a595d920c6d83d12111b13cef2502e7ed9a0
Hernan Velasquez <velasquezmhernan@gmail.com>
2022-06-15 21:03:55 -0500

    Agregando el repositorio de trabajo

---
+++ backend/src/main/java/org/sofka/backend/repository/TareaListaRepository.java
@@ -0,0 +1,7 @@
+package org.sofka.backend.repository;
+
+import org.sofka.backend.models.TareaListasModel;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TareaListaRepository extends CrudRepository<TareaListasModel, Long> {
+}
---
+++ backend/src/main/java/org/sofka/backend/repository/TarjetasRepository.java
@@ -0,0 +1,8 @@
+package org.sofka.backend.repository;
+
+import org.sofka.backend.models.TarjetasModel;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TarjetasRepository extends CrudRepository<TarjetasModel, Long> {
+
+}
